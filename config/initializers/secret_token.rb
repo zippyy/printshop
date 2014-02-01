d73edc83ee0af055cd4dbfe06e0d5241d2408d1c
Pat Hann <pathann@gmail.com>
2014-02-01 08:17:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Printshop::Application.config.secret_key_base = 'f06e70ae1eb5aa1cf31e91c19402c811e9fb6dae5bea0fe8d13a45b7a58d436c82a97d2827cf20a33679651042f1d866f3d9874dd4a69498ec0a2cd66165875a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Printshop::Application.config.session_store :cookie_store, key: '_printshop_session'
