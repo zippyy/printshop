dd31e5ec9beb0e4297080e3bf8641543f5488e8d
Pat Hann <pathann@gmail.com>
2014-02-02 19:22:18 -0500

    removed labels and styled selects

--- app/assets/javascripts/cart_items.js
+++ app/assets/javascripts/cart_items.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	
 	$('#product_id').on('change', function() {
+		$('#size_id, #size_id_label, #finish_id, #finish_id_label, #quantity_id, #quantity_id_label').remove();
 		var params = "product_id=" + $('#product_id').val();
 		  $.ajax({
 	    url: "/size_options",  
@@ -11,7 +12,9 @@ $(document).ready(function(){
 	});
 
 	$(document).on('change', "#size_id", function() {
+		$('#finish_id, #finish_id_label, #quantity_id, #quantity_id_label').remove();
 		var params = "product_id=" + $('#product_id').val();
+		  
 		  $.ajax({
 	    url: "/finish_options",  
 	    format: 'js',
@@ -21,6 +24,7 @@ $(document).ready(function(){
 	});
 
 	$(document).on('change', '#finish_id', function() {
+		$('#quantity_id, #quantity_id_label').remove()
 		var params = "product_id=" + $('#product_id').val();
 		  $.ajax({
 	    url: "/quantity_options",  
