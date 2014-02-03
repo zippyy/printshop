151daa0d7a98f1fe44a26f64cae674812b22900d
Pat Hann <pathann@gmail.com>
2014-02-03 08:55:18 -0500

    changing to form_tag

--- app/assets/javascripts/cart_items.js
+++ app/assets/javascripts/cart_items.js
@@ -1,44 +1,44 @@
 $(document).ready(function(){
 	
-	$('#product_id').on('change', function() {
+	$('#cart_item_product_id').on('change', function() {
 		$('#size_id, #size_id_label, #finish_id, #finish_id_label, #quantity_id, #quantity_id_label').remove();
-		var params = "product_id=" + $('#product_id').val();
+		var params = "product_id=" + $('#cart_item_product_id').val();
 		$.ajax({
 			url: "/size_options",
 			format: 'js',
 			type: "GET",
-			data: params,
+			data: params
 		})
 	});
 
-	$(document).on('change', "#size_id", function() {
-		$('#finish_id, #finish_id_label, #quantity_id, #quantity_id_label').remove();
-		var params = "product_id=" + $('#product_id').val();
-		$.ajax({
-			url: "/finish_options",
-			format: 'js',
-			type: "GET",
-			data: params,
-		})
-	});
+	// $(document).on('change', "#size_id", function() {
+	// 	$('#finish_id, #finish_id_label, #quantity_id, #quantity_id_label').remove();
+	// 	var params = "product_id=" + $('#product_id').val();
+	// 	$.ajax({
+	// 		url: "/finish_options",
+	// 		format: 'js',
+	// 		type: "GET",
+	// 		data: params,
+	// 	})
+	// });
 
-	$(document).on('change', '#finish_id', function() {
-		$('#quantity_id, #quantity_id_label').remove()
-		var params = "product_id=" + $('#product_id').val();
-		$.ajax({url: "/quantity_options",
-			format: 'js',
-			type: "GET",
-			data: params,
-		})
-	});
+	// $(document).on('change', '#finish_id', function() {
+	// 	$('#quantity_id, #quantity_id_label').remove()
+	// 	var params = "product_id=" + $('#product_id').val();
+	// 	$.ajax({url: "/quantity_options",
+	// 		format: 'js',
+	// 		type: "GET",
+	// 		data: params,
+	// 	})
+	// });
 
-	$(document).on('change', '#quantity_id', function() {
-		$.ajax({
-			url: "/submit_options",
-			format: 'js',
-			type: "GET",
-		})
-	});
+	// $(document).on('change', '#quantity_id', function() {
+	// 	$.ajax({
+	// 		url: "/submit_options",
+	// 		format: 'js',
+	// 		type: "GET",
+	// 	})
+	// });
 
 
 });
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,57 +7,56 @@ class CartItemsController < ApplicationController
   def new
   	@cart_item = CartItem.new
     @cart_items = CartItem.all
-  	@products = Product.all
+  	@products = Product.all.collect {|p| [p.name, p.id]}
   end
 
   def create
   	@cart_item = CartItem.new(cart_item_params)
   	
   	if @cart_item.save
-  		redirect_to cart_items_path
+  		redirect_to new_cart_items_path
   	else
   		render :new
   	end
   end
 
-  def size_options
-    @product = Product.find_by_id(params[:product_id])
-    @sizes = @product.sizes
+  # def size_options
+  #   @product = Product.find_by_id(params[:product_id])
+  #   @sizes = @product.sizes.all.collect {|p| [p.size, p.id]}
+  #   respond_to do |format|
+  #      format.js {  }
+  #   end
+  # end
 
-    respond_to do |format|
-       format.js {  }
-    end
-  end
+  # def finish_options
+  #   @product = Product.find_by_id(params[:product_id])
+  #   @finishes = @product.finishes
 
-  def finish_options
-    @product = Product.find_by_id(params[:product_id])
-    @finishes = @product.finishes
-
-    respond_to do |format|
-       format.js {  }
-    end
-  end
+  #   respond_to do |format|
+  #      format.js {  }
+  #   end
+  # end
 
 
-  def quantity_options
-    @product = Product.find_by_id(params[:product_id])
-    @quantities = @product.quantities
+  # def quantity_options
+  #   @product = Product.find_by_id(params[:product_id])
+  #   @quantities = @product.quantities
 
-    respond_to do |format|
-       format.js {  }
-    end
-  end
+  #   respond_to do |format|
+  #      format.js {  }
+  #   end
+  # end
 
-  def submit_options
-    respond_to do |format|
-       format.js {  }
-    end    
-  end
+  # def submit_options
+  #   respond_to do |format|
+  #      format.js {  }
+  #   end    
+  # end
 
   private
 
   def cart_item_params
-  	params.fetch(:cart_item, {}).permit(:product_id, :size_id, :finish_id, :quantity_i, :price_id)
+  	params.require(:cart_item).permit(:product_id, :size_id, :finish_id, :quantity_id, :price_id)
   end
 
 
