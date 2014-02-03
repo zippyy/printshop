cba6668a346afb8f45a467c29fb3991cdf0469b5
Pat Hann <pathann@gmail.com>
2014-02-03 11:34:49 -0500

    break point for undefinded f error

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,26 +7,31 @@ class CartItemsController < ApplicationController
   def new
   	@cart_item = CartItem.new
     @cart_items = CartItem.all
-  	@products = Product.all.collect {|p| [p.name, p.id]}
+  	@products = Product.all.map { |e| [e.name, e.id] }
+    # @sizes = Size.all.map { |e| [e.size, e.id] }
+    @quantities = Quantity.all.map { |e| [e.quantity, e.id] }
+    @finishes = Finish.all.map { |e| [e.finish, e.id] }
+    @f=nil
   end
 
   def create
   	@cart_item = CartItem.new(cart_item_params)
   	
   	if @cart_item.save
-  		redirect_to new_cart_items_path
+  		redirect_to new_cart_item_path
   	else
   		render :new
   	end
   end
 
-  # def size_options
-  #   @product = Product.find_by_id(params[:product_id])
-  #   @sizes = @product.sizes.all.collect {|p| [p.size, p.id]}
-  #   respond_to do |format|
-  #      format.js {  }
-  #   end
-  # end
+  def size_options
+    @product = Product.find_by_id(params[:product_id])
+    @sizes = @product.sizes.map { |e| [e.size, e.id] }
+
+    respond_to do |format|
+       format.js {  }
+    end
+  end
 
   # def finish_options
   #   @product = Product.find_by_id(params[:product_id])
