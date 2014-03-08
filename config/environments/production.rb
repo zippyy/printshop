dbc743e8c5839f8f54a1e02651b04ec8abc5ad90
Pat Hann <pathann@gmail.com>
2014-03-08 09:11:42 +0000

    capified and deploy files created

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Printshop::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -77,4 +77,6 @@ Printshop::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.relative_url_root = '/printshop'
+
 end
