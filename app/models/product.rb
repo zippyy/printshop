065b5d20688e68e8dd6bc9bb5f162a3d11b83dd9
Pat Hann <pathann@gmail.com>
2014-03-10 12:28:45 +0000

    product list with characteristics

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,11 +13,15 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :quantities, allow_destroy: true, reject_if: proc { |attributes| attributes['quantity'].blank? }
 	
 	def has_price?(size, finish, quantity)
-		Price.where(:product_id => self.id, :size_id => size.id, :finish_id => finish.id, :quantity_id => quantity.id).exists?
+		Price.where(product_id: self.id, size_id: size.id, finish_id: finish.id, quantity_id: quantity.id).exists?
 	end
 
 	def get_price(size, finish, quantity)
-		Price.find_by(:product_id => self.id, :size_id => size.id, :finish_id => finish.id, :quantity_id => quantity.id)
+		Price.find_by(product_id: self.id, size_id: size.id, finish_id: finish.id, quantity_id: quantity.id)
+	end
+
+	def has_characteristic?(object)
+		object.find_by(product_id: self.id)
 	end
 
 end
