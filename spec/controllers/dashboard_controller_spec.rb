5fbe4ac8067a48ec07389e1ed3952564d9f4cefe
Pat Hann <pathann@gmail.com>
2014-03-23 19:44:51 +0000

    dashboard controller tested

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe DashboardController do
 
+	let(:product) { create(:product) }
+
+	describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+
+    it "populates an array of products" do
+      get :index 
+      assigns(:products).should eq([product])
+    end
+  end
 end
