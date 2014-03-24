18f52477c614b1cd4bde09909163a1af9bed8791
Pat Hann <pathann@gmail.com>
2014-03-24 08:20:49 +0000

    session controller tested

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,61 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
+  describe "POST #create" do
 
+		before :each do
+      @user = create(:user)
+    end
+    
+    context "with valid credentials" do
+      it "logs a user in" do
+        post :create, { 
+          email: "pat@gmail.com",
+          password: "secret"
+        }
+        expect(response).to be_redirect
+        expect(response).to redirect_to dashboard_path
+        expect(flash[:notice]).to eq "Logged in!"
+      end
+    end
+
+    context "with invalid credentials" do
+      it "doesn't log a user in when a user doesn't exist" do
+        post :create, {
+          email: "someguy@gmail.com",
+          password: "somepass",
+        }
+        expect(response).to render_template :new
+        expect(flash[:alert]).to eq "Email or password was invalid"
+      end
+
+      it "doesn't log a user in when a password is incorrect." do
+        post :create, {
+          email: "pat@gmail.com",
+          password: "asdf",
+        }
+        expect(response).to render_template :new
+        expect(flash[:alert]).to eq "Email or password was invalid"
+      end
+
+      it "doesn't log a user in when the email is incorrect." do
+        post :create, {
+          email: "path@gmail.com",
+          password: "secret",
+        }
+        expect(response).to render_template :new
+        expect(flash[:alert]).to eq "Email or password was invalid"
+      end
+	  end
+
+	  describe "POST #destroy" do
+	    
+	    it "logs a user out" do
+	      delete :destroy
+	      expect(response).to be_redirect
+	      expect(response).to redirect_to root_path
+	      expect(flash[:notice]).to eq "Logged out!"
+	    end
+	  end
+  end
 end
