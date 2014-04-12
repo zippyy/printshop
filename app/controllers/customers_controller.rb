a789e2a573e1bf399bda0fb3809c48908c89f733
Pat Hann <pathann@gmail.com>
2014-04-12 12:04:46 +0100

    customers controller created

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,24 @@
 class CustomersController < ApplicationController
   def index
+
   end
 
   def new
+  	@customer = Customer.new
+  end
+
+  def create
+		@customer = Customer.new(customer_params)
+	  if @customer.save
+	    redirect_to dashboard_url, :notice => "Signed up!"
+	  else
+	    render :new
+	  end
+  end
+
+  private
+
+  def customer_params
+  	params.require(:customer).permit(:name, :company, :email, :email_confirmation)  	
   end
 end
